04d73bbebda016f6c8e1ec784afb03e3cd4e9667
Maarten Pijnenborg <maarten.pijnenborg@gmail.com>
2022-03-18 20:23:08 +0100

    Beta 0.0 front end implemented without security

--- PortfolioTracker/PortfolioTracker/Client/Program.cs
+++ PortfolioTracker/PortfolioTracker/Client/Program.cs
@@ -10,19 +10,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddHttpClient("PortfolioTracker.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
-#if DEBUG
-builder.Services.AddDbContext<MPortfolioDBContext>(options => {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("PaijnzzzDB"), options => options.MigrationsAssembly("PortfolioTracker.Server"));
-});
-#endif
 
-#if RELEASE
-builder.Services.AddDbContext<MPortfolioDBContext>(options => {
-    options.UseSqlServer(Environment.GetEnvironmentVariable("SQLAZURECONNSTR_PaijnzzzDB"), options => options.MigrationsAssembly("PortfolioTracker.Server"));
-    });
-#endif 
 // Supply HttpClient instances that include access tokens when making requests to the server project
-builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("PortfolioTracker.ServerAPI"));
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddMsalAuthentication(options =>
 {
--- PortfolioTracker/PortfolioTracker/Server/Controllers/PortfolioController.cs
+++ PortfolioTracker/PortfolioTracker/Server/Controllers/PortfolioController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PortfolioTracker.Implementation.Services;
+using PortfolioTracker.Model;
 
 namespace PortfolioTracker.Server.Controllers
 {
@@ -10,14 +12,28 @@ namespace PortfolioTracker.Server.Controllers
         private readonly IAssetService _assetService;
         private readonly IPortfolioService _portfolioService;
         private readonly IPortfolioHistoryService _portfolioHistoryService;
+        private readonly MPortfolioDBContext _dbContext;
 
-        public PortfolioController(IAssetService assetService, IPortfolioService portfolioService, IPortfolioHistoryService portfolioHistoryService)
+        public PortfolioController(IAssetService assetService, IPortfolioService portfolioService, IPortfolioHistoryService portfolioHistoryService, MPortfolioDBContext dBContext)
         {
             _assetService = assetService;
             _portfolioService = portfolioService;
             _portfolioHistoryService = portfolioHistoryService;
+            _dbContext = dBContext;
         }
 
+        [HttpGet]
+        public async Task<IEnumerable<Portfolio>> Get()
+        {
+            return await _dbContext.Portfolio.Include(x=>x.Asset).ToListAsync();
+        }
+
+        [HttpGet]
+        [Route("PortfolioHistory")]
+        public IEnumerable<PortfolioHistory> GetPortfolioHistory()
+        {
+            return _dbContext.PortfolioHistory;
+        }
 
         [HttpGet]
         [Route("UpdateAssets")]
@@ -36,6 +52,32 @@ namespace PortfolioTracker.Server.Controllers
             await _assetService.UpdateAssets();
             await _portfolioService.UpdatePortfolio();
             await _portfolioHistoryService.CreatePortfolioHistory();
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("CreatePortfolioHistoryOnceWithoutRealCalcluation")]
+
+        public async Task<IActionResult> CreatePortfolioHistoryOnceWithoutRealCalcluation()
+        {
+            var firstPortfolioHistory =await _dbContext.PortfolioHistory.OrderBy(x => x.Date).FirstAsync();
+
+            var transactions = _dbContext.Transactions.Where(x => x.CreatedOn < firstPortfolioHistory.Date).OrderBy(x => x.CreatedOn).ToList();
+            decimal totalValue = 0;
+            foreach (var transactionPerDate in transactions.GroupBy(x=>x.CreatedOn))
+            {
+                totalValue += transactionPerDate.Sum(x => x.TotalCosts);
+                var portfolioHistory = new PortfolioHistory
+                {
+                    Date = transactionPerDate.Key,
+                    Percentage = 0,
+                    Profit = 0,
+                    TotalInvestedPortfolioValue = totalValue,
+                    TotalPortfolioValue = totalValue
+                };
+                await _dbContext.PortfolioHistory.AddAsync(portfolioHistory);
+            }
+            await _dbContext.SaveChangesAsync();
             return Ok();
         }
     }
