89915059a00d61d8cd9a58d6dbf1090f370091e1
PaijnzzzPC <maarten.pijnenborg@gmail.com>
2022-03-31 20:38:55 +0200

    alle hardcoded features krijgen waarde vanuit database

---
+++ PortfolioTracker/PortfolioTracker/Server/Controllers/TransactionController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PortfolioTracker.Implementation.Resolvers;
+using PortfolioTracker.Implementation.Services;
+using PortfolioTracker.Model;
+
+namespace PortfolioTracker.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TransactionController : Controller
+    {
+        private readonly MPortfolioDBContext _dbContext;
+        
+        public TransactionController( MPortfolioDBContext dBContext)
+        {
+            _dbContext = dBContext;
+        }
+
+
+        [HttpGet]
+        [Route("TotalTransactionCosts")]
+        public async Task<decimal> GetTotalTransactionCosts() => await _dbContext.Transactions.SumAsync(x => x.TransactionCosts);
+        
+
+        [HttpGet]
+        [Route("TotalTaxes")]
+        public async Task<decimal> GetTotalTaxes() => await _dbContext.Transactions.SumAsync(x => x.TaxesCosts);
+
+    }
+}
