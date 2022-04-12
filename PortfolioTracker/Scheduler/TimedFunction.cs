eae6db176425bedda8023d4664fe7662a1488678
Maarten Pijnenborg <maarten.pijnenborg@vecozo.nl>
2022-04-12 15:05:25 +0200

    Azure functions introduceren om timed requests te kunnen versturen

---
+++ PortfolioTracker/Scheduler/TimedFunction.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using PortfolioTracker.Scheduler.API;
+
+namespace PortfolioTracker.Scheduler
+{
+    public class TimedFunction
+    {
+        [FunctionName("CreatePortfolioHistory")]
+        public async Task Run([TimerTrigger("0 18 * * *")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            var client = new PortfolioTrackerClient("https://portfoliotrackerpaijnzzz.azurewebsites.net/", new HttpClient());
+            await client.CreatePortfolioHistoryAsync(); ;
+        }
+    }
+}
