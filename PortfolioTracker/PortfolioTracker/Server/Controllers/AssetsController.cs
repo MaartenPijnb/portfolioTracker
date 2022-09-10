7c336535bfe061a1404f5a6cc47c20cd8d9a2e50
Maarten Pijnenborg <maarten.pijnenborg@gmail.com>
2022-09-10 20:51:03 +0200

    Added page to view all assets

---
+++ PortfolioTracker/PortfolioTracker/Server/Controllers/AssetsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PortfolioTracker.Implementation.Services;
+using PortfolioTracker.Model;
+
+namespace PortfolioTracker.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AssetsController : Controller
+    {
+        private readonly MPortfolioDBContext _dbContext;
+        private readonly IAssetService _assetService;
+
+        public AssetsController(MPortfolioDBContext dbContext, IAssetService assetService)
+        {
+            _dbContext = dbContext;
+            _assetService = assetService;
+        }
+
+
+        [HttpGet]
+        public async Task<List<Asset>> GetAll() => await _dbContext.Assets.ToListAsync();
+
+        [Route("UpdateAllAssets")]
+        [HttpPost]
+        public async Task UpdateAllAssets()
+        {
+            await _assetService.UpdateAssets();
+        }
+    }
+}
